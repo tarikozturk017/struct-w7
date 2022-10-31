44ae0f007195f327f136bfadbcbdfbc0510bae0c
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-10-31 11:44:48 -0400

    Add files via upload

---
+++ closing.c
@@ -0,0 +1,20 @@
+/**********************
+* AUTHOR: Tarik Ozturk
+* Date: 2022/10/30
+* *********************/
+
+#include <stdio.h>
+
+// structure here
+
+int main()
+{
+    //complete the code here
+
+    for (int i = 0; i < 2; i++) {
+        printf("position: %s, date started: %s, salary: %d\n",
+            employees[i].position, employees[i].dateStarted, employees[i].salary);
+    }
+
+    return 0;
+}
